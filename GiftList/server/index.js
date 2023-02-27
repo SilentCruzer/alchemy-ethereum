5cd7541fedc3565e302946fb0c6ac2aa1f5405d7
SilentCruzer <radithyakumar7@gmail.com>
2023-02-27 19:25:29 +0530

    Implements prook verification

--- GiftList/server/index.js
+++ GiftList/server/index.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const niceList = require('../utils/niceList.json')
+const MerkleTree = require("../utils/MerkleTree")
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = new MerkleTree(niceList).getRoot();
 
 app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
+  const {name , proof} = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof(proof,name, MERKLE_ROOT);
   if(isInTheList) {
-    res.send("You got a toy robot!");
+    res.send("You got a toy rSobot!");
   }
   else {
     res.send("You are not on the list :(");
